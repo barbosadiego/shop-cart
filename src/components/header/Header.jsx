bde34785457120f19ce486877d2a360c2caab97d
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-17 14:52:07 -0300

    create header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <input type="text" placeholder="Search..." />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
