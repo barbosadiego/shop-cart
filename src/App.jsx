4dfb06759bd6f2a44b8c7cc2e2ca1b5df1ae8333
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-17 14:50:22 -0300

    app component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Produtc from './pages/product/ProductPage';
+import Cart from './pages/cart/CartPage';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
 export default function App() {
   return (
     <>
-      <h1>ola mundo</h1>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Produtc />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   );
 }
