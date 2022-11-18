a7eee5498295f2abc8e3052a584f1f80a00095f0
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-18 15:51:15 -0300

    create cart page component

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import { useContext, useState } from 'react';
+import plus from '../../assets/plus.svg';
+import deleteIcon from '../../assets/delete.svg';
 
-const CartPage = (props) => {
+import './CartPage.scss';
+import { CartContext } from '../../contexts/CartContext';
+
+const CartPage = () => {
+  const { cart, increaseQuantity, removeFromCart } = useContext(CartContext);
+
   return (
-    <div>
-      <p>cart</p>
-    </div>
+    <section className="cart-page container">
+      <div className="cart-page__items">
+        {cart.length === 0 && <p>no items to display</p>}
+        {cart &&
+          cart.map((item, index) => {
+            return (
+              <div className="item" key={index}>
+                <img src={item.image} alt={item.title} />
+                <div className="info">
+                  <h4 className="title">{item.title}</h4>
+                  <span className="price">${item.price * item.qty}</span>
+                  <div className="qty">
+                    <button onClick={() => removeFromCart(item.id)}>
+                      <img src={deleteIcon} />
+                    </button>
+                    <span>{item.qty}</span>
+                    <button onClick={() => increaseQuantity(item)}>
+                      <img src={plus} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </section>
   );
 };
 
