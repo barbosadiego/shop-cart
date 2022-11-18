8c7f7bcdedd622c24e0dce6ada6e302a9e85322d
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-18 15:48:37 -0300

    add context to app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './App.scss';
-import App from './App';
 import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+
+import './App.scss';
+
+import { CartProvider } from './contexts/CartContext';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
   </StrictMode>,
 );
