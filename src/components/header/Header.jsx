c43a98f43affb21dacc8cbd7b91b67a9eb5ab73c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-19 11:40:59 -0300

    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,14 +6,18 @@ import './Header.scss';
 const Header = () => {
   return (
     <header className="header">
-      <div className="container">
-        <Link to="/" className="logo">
-          FashionShop
-        </Link>
-        <Link to="/cart">
-          <img src={bag} alt="" width={30} height={30} />
-        </Link>
-      </div>
+      <ul className="container">
+        <li>
+          <Link to="/" className="logo">
+            OnlineShop
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            <img src={bag} alt="" width={30} height={30} />
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 };
