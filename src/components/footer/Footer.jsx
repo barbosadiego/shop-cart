2ab1214eb1002f46bc11c93218a322144ca550dd
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-19 11:40:41 -0300

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,8 @@
 import './Footer.scss';
 const Footer = () => {
-  return <footer className="footer">All rights reserved &copy;2022</footer>;
+  return (
+    <footer className="footer container">All rights reserved &copy;2022</footer>
+  );
 };
 
 export default Footer;
