2a8fa317bea9df64f30db3978bf93b954ff0ce9f
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-19 11:41:13 -0300

    home page created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import useApi from '../hooks/useApi';
+import Card from '../components/card/Card';
+import './HomePage.scss';
 
-const HomePage = (props) => {
-  return <div>home</div>;
+const HomePage = () => {
+  const [products, setProducts] = useState([]);
+  const api = useApi();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await api.getProducts();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <section className="home">
+      <div className="container">
+        <h1>New Arrivals</h1>
+        <div className="home__grid">
+          {products &&
+            products.map((item, index) => {
+              return <Card item={item} key={index} />;
+            })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
