19213d23b3af9ca2994c4b69a7fb4eb8b46acaf9
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-19 21:27:17 -0300

    add cart component structure

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -7,41 +7,52 @@ import './CartPage.scss';
 
 const CartPage = () => {
   const { cart, clearCart } = useContext(CartContext);
-  // console.log(cart);
+
+  function calculateTotal() {
+    let amount = 0;
+    cart.forEach((item) => {
+      amount += item.price * item.qty;
+    });
+    return amount;
+  }
 
   return (
     <section className="cart-page">
       <div className="container">
         <h1>Shooping Cart</h1>
-        <div className="cart-page__table">
-          <div className="t-head grid">
-            <p>product</p>
-            <p>price</p>
-            <p>quantity</p>
-            <p>total</p>
-          </div>
-          <div className="t-body">
-            {cart &&
-              cart.map((item, index) => {
-                return <TableItem item={item} key={index} />;
-              })}
-          </div>
-          <div className="subtotal">
-            <button className="btn" onClick={clearCart}>
-              Clear Cart
-            </button>
-            <div>
-              <div className="total-price">
-                <h3>Subtotal:</h3>
-                <span>
-                  <strong>0</strong>
-                </span>
+        {cart.length ? (
+          <div className="cart-page__table">
+            <div className="t-head grid">
+              <p>product</p>
+              <p>price</p>
+              <p>quantity</p>
+              <p>total</p>
+            </div>
+            <div className="t-body">
+              {cart &&
+                cart.map((item, index) => {
+                  return <TableItem item={item} key={index} />;
+                })}
+            </div>
+            <div className="subtotal">
+              <button className="btn" onClick={clearCart}>
+                Clear Cart
+              </button>
+              <div>
+                <div className="total-price">
+                  <h3>Subtotal: $ </h3>{' '}
+                  <span>
+                    <strong>{calculateTotal().toFixed(2)}</strong>
+                  </span>
+                </div>
+                <button className="btn checkout">checkout</button>
+                <Link to="/">Continue shopping</Link>
               </div>
-              <button className="btn checkout">checkout</button>
-              <Link to="/">Continue shopping</Link>
             </div>
           </div>
-        </div>
+        ) : (
+          <h3>There is no items to show here... :-(</h3>
+        )}
       </div>
     </section>
   );
